9d8d5abc5e8c2f33e9eabd12c43c1e021284c70c
RepoRizuku <142154152+rizuku-v2@users.noreply.github.com>
2025-01-26 09:53:29 +0700

    Update script.js

--- SDN-Sukatani-1-Depok/script.js
+++ SDN-Sukatani-1-Depok/script.js
@@ -1,14 +1,4 @@
 // File: script.js
-
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Modern Website Loaded!");
-
-    // Ambil elemen <p> yang memiliki id 'footer-copyright'
-    const copyrightElement = document.getElementById('footer-copyright');
-    
-    // Dapatkan tahun saat ini
-    const currentYear = new Date().getFullYear();
-    
-    // Ubah konten teks elemen tersebut dengan tahun otomatis
-    copyrightElement.innerHTML = `&copy; ${currentYear} SDN Sukatani 1. All rights reserved.`;
 });
