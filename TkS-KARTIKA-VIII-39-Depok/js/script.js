107c2f39f252bd1b809e66fef3ef869a7474b834
RepoRizuku <142154152+rizuku-v2@users.noreply.github.com>
2025-02-02 08:12:42 +0700

    Update script.js

--- TkS-KARTIKA-VIII-39-Depok/js/script.js
+++ TkS-KARTIKA-VIII-39-Depok/js/script.js
@@ -55,3 +55,29 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// Get the pop-up
+var popup = document.getElementById("referencesPopup");
+
+// Get the button that opens the pop-up
+var btn = document.getElementById("referencesButton");
+
+// Get the <span> element that closes the pop-up
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the pop-up 
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the pop-up
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the pop-up, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
